5e141b33f8ae05fb1b67a1cfbb8923f53e6ac94a
RomyCardozo <romycardozopy@gmail.com>
2025-10-05 20:23:19 -0300

    Implementa la función de eliminación de tareas con base de datos, validación de ID y manejo de errores.

--- app.js
+++ app.js
@@ -123,8 +123,42 @@ app.put("/api/todos/:id", async (req, res) => {
 })
 
 
+app.delete("/api/todos/:id", async (req, res) => {
+    const id = parseInt(req.params.id);//obtenemos valor de la url, y parseamos a entero
+    //validacion de si el id es un numero
+    if (Number.isNaN(id)) {
+        return res.status(400).json({
+            ok: false,
+            message: "Id invalido"
+        })
+    }
 
-app.delete("/api/todos/:id", (req, res) => {
+    try {
+        const tareaEliminada = await db.oneOrNone(`DELETE FROM tareas
+	WHERE id= $1 RETURNING *`, [id]);
+        if (!tareaEliminada) {//trae none si no existe el id
+            return res.status(404).json({
+                ok: false,
+                message: `Error no existe la tarea con id: ${id}`
+            })
+        }
+        res.status(200).json({
+            ok: true,
+            message: `Tarea con id: ${id} eliminada con exito`
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            message: `Error al intentar eliminar la tarea con id: ${id}`
+        })
+    }
+
+})
+
+
+
+/*app.delete("/api/todos/:id", (req, res) => {
     const id = parseInt(req.params.id);//obtenemos valor del parametro y parseamos a entero
     let tareaEliminada = {}; //declaramos variable vacia
     const tareaIndice = todos.findIndex((value) => value.id === id);//enoctramos indice de la tarea
@@ -138,7 +172,7 @@ app.delete("/api/todos/:id", (req, res) => {
         message: "Tarea eliminada",
         data: tareaEliminada
     })
-})
+})*/
 
 
 app.listen(PORT, () => console.log("Ejecutandose en el puerto " + PORT))

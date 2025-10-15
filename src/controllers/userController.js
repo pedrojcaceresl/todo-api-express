4b5c1c75130bec873059b65987743980ff228a40
RomyCardozo <romycardozopy@gmail.com>
2025-10-15 00:22:57 -0300

    Agrega funcionalidad de inicio de sesión de usuarios en userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config(); // carga variables de .env
+
 import User from "../models/User.js"
+import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 console.log("Este es: " + JWT_SECRET);
@@ -50,3 +54,54 @@ export const registerUser = async (req, res) => {
     }
 
 }
+
+//funcion login
+
+export const userLogin = async (req, res) => {
+    const { email, contraseña } = req.body;
+    if (!email || !contraseña) {
+        return res.status(400).json({
+            ok: false,
+            message: "Error campos vacios"
+        })
+    }
+    try {
+
+        const user = await User.findOne({ where: {email} });
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                message: "Error el usuario con ese email no esta registrado"
+            })
+        }
+        if (contraseña !== user.contraseña) {
+            return res.status(400).json({
+                ok: false,
+                message: "Error contraseña o usuario incorrectos"
+            })
+        }
+
+        //verificamos si existe la clave para firmar
+        if (!JWT_SECRET) {
+            return res.status(500).json({
+                ok: false,
+                message: "Error de configuracion del servidor"
+            })
+        }
+        //creacion de token para el usuario -> jwt.sign(payload, secretOrPrivateKey, [options])
+        const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET,  { expiresIn: "1h" });
+        res.status(200).json({
+            ok: true,
+            message: "Usuario logueado con exito",
+            data: token
+        })
+        
+
+    } catch (error) {
+        console.error("El error es : " + error);
+        res.status(500).json({
+            ok: false,
+            message: "Error en el servidor 500"
+        })
+    }
+} 
